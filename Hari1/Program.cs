ee039f36c2999b80149e313d563957e3d2f685bc
Deri Susanto <derisusanto@gmail.com>
2026-01-13 13:45:36 +0700

    day one foobar

---
+++ Hari1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NamaProyek
+{
+ class Program
+   {
+       static void Main(string[] args)
+       {
+         int maxNumber = 15;
+
+         for(int i = 1; i<= maxNumber; i++)
+         {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+            Console.WriteLine("FooBar");
+            }else if(i % 3 == 0)
+            {
+               Console.WriteLine("Foo");
+            }else if(i % 5 == 0)
+            {
+               Console.WriteLine("Bar");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+         }
+       }
+   }
+}
