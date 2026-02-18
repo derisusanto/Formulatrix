d41961b6bc8ad22384a471a4a48140043547d287
Deri Susanto <derisusanto@gmail.com>
2026-02-18 14:57:19 +0700

    s

--- otheloUI/obj/Debug/net8.0/OthelloUI.AssemblyInfo.cs
+++ otheloUI/obj/Debug/net8.0/OthelloUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OthelloUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25017c7d5be15d9d840df777a3af8dff1400a474")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d51601c5640eb40f28b1185f975a0979091cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OthelloUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OthelloUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
